f54ade2c93d303b389c7197567d24c957bc8584f
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-05-26 20:13:18 -0300

    actualizado el 26/05

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+
+int num_invertido;
+string num_leido;
+
+Console.WriteLine("Ingrese un número");
+//Console.ReadLine(num_leido);
