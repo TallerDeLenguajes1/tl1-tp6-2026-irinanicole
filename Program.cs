25565cbad4a54466d277e637f05de9e2d35de1fe
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-06-01 23:38:19 -0300

    Ejercicio 1: completado

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
-
-int num_invertido;
-string num_leido;
+﻿int num_leido;
+string? s;
 
 Console.WriteLine("Ingrese un número");
-//Console.ReadLine(num_leido);
+s = Console.ReadLine();
+bool esUnNumero = int.TryParse(s, out num_leido);
+if (esUnNumero)
+{
+    int invertido = 0;
+    while(num_leido > 0)
+    {
+        int digito = num_leido % 10;
+        invertido = (invertido * 10) + digito;
+        num_leido = num_leido / 10;
+    }
+    Console.WriteLine($"Numero Invertido: {invertido}");
+}
