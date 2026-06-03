54f8329e7cab04ebce689bb358e43569175c8b02
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-06-02 22:11:54 -0300

    Ejercicio 4: comenzado y pendiente

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-﻿int num_leido;
-string? s;
-
-Console.WriteLine("Ingrese un número");
-s = Console.ReadLine();
-bool esUnNumero = int.TryParse(s, out num_leido);
-if (esUnNumero)
-{
-    int invertido = 0;
-    while(num_leido > 0)
-    {
-        int digito = num_leido % 10;
-        invertido = (invertido * 10) + digito;
-        num_leido = num_leido / 10;
-    }
-    Console.WriteLine($"Numero Invertido: {invertido}");
-}
+﻿// EJERCICIO 4
+/* Dada una cadena (un string) de texto ingresada por el usuario, realice las siguientes tareas */
+Console.WriteLine("Ingrese un palabra:");
+string? cadena = Console.ReadLine();
+Console.WriteLine("Tenkiu!");
